51a912431640eeff2a2ddef1123e1ecaa57e2a01
mutlusun <mutlusun@users.noreply.github.com>
2024-11-28 15:31:03 +0100

    fix

--- package/scripts/linux/appimage/build-appimage.sh
+++ package/scripts/linux/appimage/build-appimage.sh
@@ -10,10 +10,8 @@ mkdir $OUTPUT_DIR
 cp -T -r ./package/pkg-working/ $OUTPUT_DIR/usr/
 
 # Add icon and desktop file.
-mkdir -p $OUTPUT_DIR/usr/share/icons
-cp ./package/scripts/linux/appimage/quarto.svg $OUTPUT_DIR/usr/share/icons/
-mkdir -p $OUTPUT_DIR/usr/share/applications/
-cp ./package/scripts/linux/appimage/quarto.desktop $OUTPUT_DIR/usr/share/applications/
+cp ./package/scripts/linux/appimage/quarto.svg $OUTPUT_DIR/
+cp ./package/scripts/linux/appimage/quarto.desktop $OUTPUT_DIR/
 
 # Add AppRun file
 cp ./package/scripts/linux/appimage/AppRun $OUTPUT_DIR/AppRun
@@ -22,5 +20,4 @@ chmod a+x $OUTPUT_DIR/AppRun
 # Get and run appimagetool
 wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
 chmod a+x ./appimagetool-x86_64.AppImage
-sudo apt install desktop-file-utils
 ARCH=$TARGET_ARCH ./appimagetool-x86_64.AppImage $OUTPUT_DIR
