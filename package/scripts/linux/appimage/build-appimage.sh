46b64f9cb6ddb139bba944e36abab94357318673
mutlusun <mutlusun@users.noreply.github.com>
2024-11-28 15:03:43 +0100

    fix

--- package/scripts/linux/appimage/build-appimage.sh
+++ package/scripts/linux/appimage/build-appimage.sh
@@ -6,7 +6,7 @@ OUTPUT_DIR="./AppDir"
 rm -rf $OUTPUT_DIR | true
 
 # Make appdir and copy quarto files.
-mkdir .$OUTPUT_DIR
+mkdir $OUTPUT_DIR
 cp -T -r ./package/pkg-working/ $OUTPUT_DIR/usr/
 
 # Add icon and desktop file.
@@ -21,4 +21,5 @@ chmod a+x $OUTPUT_DIR/AppRun
 
 # Get and run appimagetool
 wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod a+x ./appimagetool-x86_64.AppImage
 ARCH=$TARGET_ARCH ./appimagetool-x86_64.AppImage $OUTPUT_DIR
